9254068aa9dd3168ebeeabe31f22799840946f6b
Wagner Manko <wagner.manko@gmail.com>
2026-05-06 13:08:17 -0300

    estudos 06-05-2026

---
+++ src/app/Home.tsx
@@ -0,0 +1,21 @@
+import { ButtonIcon } from "@/components/ButtonIcon";
+import { Header } from "@/components/Header";
+import { Title } from "@/components/Title";
+import { View } from "react-native";
+
+export function Home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        padding: 32,
+        paddingTop: 64,
+      }}
+    >
+      <Header>
+        <Title>Home</Title>
+        <ButtonIcon name="add-circle" />
+      </Header>
+    </View>
+  );
+}
